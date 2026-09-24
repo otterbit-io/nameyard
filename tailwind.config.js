b48c97287c0d07c49886be77a682a2ad383747fd
Christoph <christoph.rustige@proton.me>
2026-09-24 11:22:58 +0200

    stripped everything except dist

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  // Only classes used in these files end up in main.css.
-  content: ['./dist/**/*.html', './dist/assets/js/**/*.js'],
-  theme: { extend: {} },
-  plugins: [],
-};
